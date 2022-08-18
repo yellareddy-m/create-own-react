43e8d7c17da8f898e0dcdb5d851e9bbd2bc4486a
YellaReddy <yem@softwareag.com>
2022-08-18 12:28:53 +0530

    added js version of jsx

--- index.js
+++ index.js
@@ -53,5 +53,12 @@ const element = (
     </div>
 )
 
+// alertnative to above jsx
+/*
+const element = Didact.createElement("div", {
+  id: "foo"
+}, Didact.createElement("a", null, "bar"), Didact.createElement("b", null));
+ */
+
 const container = document.getElementById("root");
 Didact.render(element, container);
